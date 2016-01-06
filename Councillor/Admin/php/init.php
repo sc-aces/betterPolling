028e0aef0326b0a5a0c3eb5f784648c260b98b93
Daniel Lennox <dlennox2424@gmail.com>
2016-01-05 22:08:17 -0700

    Creating file structure with default files
    File Structure:
        Councillor
            Admin
                js
                php
            Client
                js
                php
        Election
            Admin
                js
                php
            Client
                js
                php
        sql init

---
+++ Councillor/Admin/php/init.php
@@ -0,0 +1,6 @@
+/*
+	Created for the Association Of Capitalists, Explorers, & Soldiers (ACES) 
+	by Daniel Lennox(MobiusKiller)
+	Jan 5 2016
+	betterPolling
+*/
---
+++ Election/Client/js/main.js
@@ -0,0 +1,4 @@
+//	Created for the Association Of Capitalists, Explorers, & Soldiers (ACES) 
+//	by Daniel Lennox(MobiusKiller)
+//	Jan 5 2016
+//	betterPolling
